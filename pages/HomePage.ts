ada0f33f2e709f793628bdfb4bff6ff38543670b
sbdsh <vladislavsabadash@gmail.com>
2024-09-09 19:28:40 +0700

    review fixes

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -1,22 +1,13 @@
 import { BasePage } from "./BasePage";
-import { Page } from "playwright/test";
 
 export class HomePage extends BasePage {
    private cwcText = this.page.locator('#cw-title');
    private cookiesButton = this.page.locator('#cwc-accept');
    private videoThumbs = this.page.locator('[data-el="Thumb"]');
 
-   constructor(page: Page) {
-      super(page);
-   }
-
    async acceptAgeAndCookies() {
-      if (await this.cwcText.isVisible()) {
-         await this.cwcText.click()
-      }
-      if (await this.cookiesButton.isVisible()) {
-         await this.cookiesButton.click()
-      }
+      await this.cwcText.click()
+      await this.cookiesButton.click()
    }
 
    async getRandomVideoThumbnail() {
--- pages/VideoPage.ts
+++ pages/VideoPage.ts
@@ -1,29 +1,31 @@
 import { BasePage } from './BasePage';
-import { Page, expect } from '@playwright/test';
+import { expect } from '@playwright/test';
 
 export class VideoPage extends BasePage {
-   private videoPurchaseMenuClosed = this.page.locator('.video-purchase__menu_closed .video-purchase__menu-title:text("Get full video")').first();
-   private videoElement = this.page.locator('.video__main video');
- 
-   constructor(page: Page) {
-     super(page);
-   }
- 
-   async checkVideoPurchaseMenuClosed() {
-     await expect(this.videoPurchaseMenuClosed).toBeVisible();
-   }
- 
-   async isVideoPlaying() {
-     return await this.page.evaluate(() => {
-       const videoElement = document.querySelector('video');
-       return videoElement && !videoElement.paused && videoElement.currentTime > 0;
-     });
-   }
- 
-   async isVideoMuted() {
-     return await this.page.evaluate(() => {
-       const videoElement = document.querySelector('video');
-       return videoElement ? videoElement.muted : false;
-     });
-   }
+  private videoPurchaseMenuClosed = this.page.locator('.video-purchase__menu_closed .video-purchase__menu-title:text("Get full video")').first();
+
+  async checkVideoPurchaseMenuClosed() {
+    await expect(this.videoPurchaseMenuClosed, 'Video purchase menu is not closed').toBeVisible();
+  }
+
+  async isVideoPresented() {
+    return await this.page.evaluate(() => {
+      const videoElement = document.querySelector('video');
+      return videoElement !== null;
+    });
+  }
+
+  async isVideoPlaying() {
+    return await this.page.evaluate(() => {
+      const videoElement = document.querySelector('video');
+      return videoElement && !videoElement.paused && videoElement.currentTime > 0;
+    });
+  }
+
+  async isVideoMuted() {
+    return await this.page.evaluate(() => {
+      const videoElement = document.querySelector('video');
+      return videoElement ? videoElement.muted : false;
+    });
+  }
 }
